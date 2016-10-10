a8729f041253c89c8684894fe6fb21a301e23e0c
Nikita <Nagibator666@gmail.com>
2016-10-10 08:17:36 -0700

    wow, now its done

--- weather.sh
+++ weather.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+TIMEOUT=$(grep "TIMEOUT=" ./config.ini | grep -o "[0-9]*")
 
 while [ true ]
 do
     rm index.html
     wget -q	 http://meteo.by/minsk/
-    echo `head -n 840 index.html | tail -n 1 -f `
+    echo `head -n 266 index.html | tail -n 1 `
+    sleep $TIMEOUT
 done
 
