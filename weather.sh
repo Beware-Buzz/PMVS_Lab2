5fd6d8266518a417dba54bc99057252217cd4b3e
Nikita <Nagibator666@gmail.com>
2016-10-10 08:20:47 -0700

    print working normaly

--- weather.sh
+++ weather.sh
@@ -3,7 +3,7 @@ TIMEOUT=$(grep "TIMEOUT=" ./config.ini | grep -o "[0-9]*")
 
 while [ true ]
 do
-    rm index.html
+    rm  -f index.html
     wget -q	 http://meteo.by/minsk/
     echo `head -n 266 index.html | tail -n 1 `
     sleep $TIMEOUT
