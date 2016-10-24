b8e1c8721e954087d4b312b7d82f0bf2d81935d3
Nikita <Nagibator666@gmail.com>
2016-10-24 06:23:17 -0700

    Aded No warnings

--- weather.sh
+++ weather.sh
@@ -3,9 +3,10 @@ TIMEOUT=$(grep "TIMEOUT=" ./config.ini | grep -o "[0-9]*")
 
 while [ true ]
 do
-    rm  -f index.html
+    rm  -f index.html -f
     wget -q	 http://meteo.by/minsk/
-    echo `head -n 266 index.html | tail -n 1 `
+    echo `grep '<p class="t ">' -A 6 index.html | tail -n 1 | awk {'print $1'}`
+
     sleep $TIMEOUT
 done
 
